fdf6cc740a35b17473f3581b1458a2342128bcc2
jainsakshi22 <jainsakshi22@gmail.com>
2014-11-04 02:08:56 +0530

    Created project

---
+++ Drawing a house/DrawingHouseView.h
@@ -0,0 +1,13 @@
+//
+//  DrawingHouseView.h
+//  Drawing a house
+//
+//  Created by Sakshi Jain on 04/11/14.
+//
+//
+
+#import <UIKit/UIKit.h>
+
+@interface DrawingHouseView : UIView
+
+@end
---
+++ Drawing a house/DrawingHouseView.m
@@ -0,0 +1,21 @@
+//
+//  DrawingHouseView.m
+//  Drawing a house
+//
+//  Created by Sakshi Jain on 04/11/14.
+//
+//
+
+#import "DrawingHouseView.h"
+
+@implementation DrawingHouseView
+
+/*
+// Only override drawRect: if you perform custom drawing.
+// An empty implementation adversely affects performance during animation.
+- (void)drawRect:(CGRect)rect {
+    // Drawing code
+}
+*/
+
+@end
